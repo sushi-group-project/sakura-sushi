46a0b521b7c07e4bbe53439afe0cacc1d4fbe113
Maggie Martin <maggierozay@gmail.com>
2014-10-19 20:18:11 -0400

    editing boces

--- app/scripts/boxes.js
+++
@@ -1,7 +0,0 @@
-var p=$('.blog.post');
-var divh=$('.blog').height();
-while ($(p).outerHeight()>divh) {
-    $(p).text(function (index, text) {
-        return text.replace(/\W*\s(\S)*$/, '...');
-    });
-}
